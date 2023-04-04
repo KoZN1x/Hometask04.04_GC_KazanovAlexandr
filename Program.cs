d16c4a7c7cbb2f6aa0e28a5ea95ff0dd908b6b60
Alexandr Kazanov <kazanov.aleks@gmail.com>
2023-04-04 19:25:12 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json;
+
+namespace Hometask04._04_GC_KazanovAlexandr
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var data = new JsonHandler("check.js");
+            await data.WriteJsonAsync(new {Name = "John", SecondName = "Doe"});
+            var res = await data.ReadJsonAsync<dynamic>();
+            Console.WriteLine(res);
+        }
+    }
+
+    public class JsonHandler
+    {
+        private readonly string _jsonpath;
+
+        public JsonHandler(string jsonpath)
+        {
+            _jsonpath = jsonpath;
+        }
+
+        public async Task WriteJsonAsync<T>(T data)
+        {
+            using (var serializer = new FileStream(_jsonpath, FileMode.OpenOrCreate))
+            {
+                await JsonSerializer.SerializeAsync(serializer, data);
+            }
+        }
+
+        public async Task<T> ReadJsonAsync<T>()
+        {
+            using (var serializer = new FileStream(_jsonpath, FileMode.Open))
+            {
+                return await JsonSerializer.DeserializeAsync<T>(serializer);
+            }
+        }
+    }
+        
+
+}
---
+++ bin/Debug/net6.0/check.js
@@ -0,0 +1,1 @@
+{"Name":"John","SecondName":"Doe"}
